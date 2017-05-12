6a60359648de90377808a70ed3e4591d3a710e06
WERN WONG <wernsen@w.local>
2017-05-13 06:43:56 +1200

    added morgan detailed logs on prod and env vars closes #4

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
-
+const morgan = require('morgan')
 const api = require('./server/routes/user.route');
 const apiTest = require('./server/routes/test.route');
 const app = express();
+require('dotenv').config()
 
 // Parsers for POST data
 app.use(bodyParser.json());
@@ -15,6 +16,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Point static path to dist
 app.use(express.static(path.join(__dirname, 'dist')));
 
+if(process.env.NODE_ENV !== 'dev'){
+  app.use(morgan('combined')); //'combined' outputs the Apache style LOGs
+}
 
 app.use('/api/user', api);
 app.use('/api/test', apiTest);
