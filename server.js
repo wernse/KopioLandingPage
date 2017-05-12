97ea8ff73caeee864be6fe9aacd85f74636d6c70
WERN WONG <wernsen@w.local>
2017-05-13 07:19:08 +1200

    added mongo and mongoose closes #5

--- server.js
+++ server.js
@@ -7,11 +7,24 @@ const morgan = require('morgan')
 const api = require('./server/routes/user.route');
 const apiTest = require('./server/routes/test.route');
 const app = express();
+let config = require('./server/config.js');
+const mongoose = require('mongoose');    
+
 require('dotenv').config()
 
+let options = { 
+                server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } 
+              }; 
+
+//db connection      
+console.log(config.mongodb.url)
+mongoose.connect(config.mongodb.url, options);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
 // Parsers for POST data
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ type: 'application/json'}));
 
 // Point static path to dist
 app.use(express.static(path.join(__dirname, 'dist')));
@@ -39,3 +52,6 @@ app.set('port', port);
  */
 const server = http.createServer(app);
 server.listen(port, () => console.log(`API running on localhost:${port}`));
+
+
+module.exports = app; // for testing
---
+++ server/config.js
@@ -0,0 +1,9 @@
+var config = {};
+
+config.mongodb = {};
+
+
+config.mongodb.url = "mongodb://admin:test@ds137291.mlab.com:37291/kopio";
+
+
+module.exports = config;
